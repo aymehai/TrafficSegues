16d5bc7ddc5d65bb4a38f3718a87940d4d34b361
MehSwitch <aymehaiDev@protonmail.com>
2018-06-27 16:20:02 -0400

    Initial Commit

---
+++ TrafficSegues/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TrafficSegues
+//
+//  Created by Aymen on 6/27/18.
+//  Copyright © 2018 Aymen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
